ad9658ab093f0ccb93a23f3dd1c321893a598c64
Fabian Pijcke <fabian.pijcke@gmail.com>
2018-01-19 09:53:47 +0100

    Ordering atoms for rewriting.

---
+++ rewriting.sql
@@ -0,0 +1,21 @@
+-- sqlag - Computes attack graphs (relational database theory) in SQL.
+-- Copyright (C) 2018 Fabian Pijcke
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+CREATE VIEW v_atom_rew_order AS
+SELECT qry_id, atm_id, row_number() OVER (PARTITION BY qry_id ORDER BY atm_stratum, atm_id) AS atm_rew_order
+FROM v_query_rewritable
+NATURAL JOIN v_atom_stratum;
+
